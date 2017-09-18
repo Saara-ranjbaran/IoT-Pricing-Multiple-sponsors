25505f448bc4d9117f1b0a208e0f5fe78d74fddd
Alireza khosraviyan <alirezainfotech@Alirezas-MacBook-Pro.local>
2017-09-18 17:42:40 +0430

    kncsdj

---
+++ pro.py
@@ -0,0 +1,157 @@
+import threading
+from enum import Enum
+from random import randint as rt
+import sys
+class Type(Enum):
+	A = 0
+	B = 1
+	C = 2
+	D = 3
+
+class APPType(Enum):
+	A = 0
+	B = 1
+	C = 2
+	D = 3
+
+class Sponsor(threading.Thread):
+	"""
+
+
+	"""
+	self.ID = randint(0,999999)
+	self.type = APPType.A
+	self.application_thread_pool = []
+	self.things_thread_pool = []
+
+	pass
+
+
+class Apllication(threading.Thread):
+	"""
+
+	"""
+	self.type = APPType.A
+	self.things_thread_pool = []
+
+	def __init__(self):
+		self.evaluate()
+
+	def evaluate(self):
+		for cur in things_thread_pool:
+			cur.value = rt(1 , 11)
+
+	# def __gt__(self):
+	# 	pass
+
+class Thing(threading.Thread):
+	"""
+
+	"""
+	self.ID = randint(0,999999)
+	self.type = Type.A
+	self.value = 0
+
+
+
+def Terminal(sponsors):
+	if len(sponsors) == 0:
+		print("its suppoed you created at leat one sponsor!")
+		return
+	sponsor = sponsors[0]
+	bash = ""
+	print ("IoT command $ ")
+	while True:
+		try:
+			print("command $ ")
+			if bash == "":
+				continue
+			elif bash == "topo":
+				apps = sorted(sponsor.application_thread_pool)
+				for cur in apps
+					print ("Application \"" + cur.ID + "\" has following thins : ")
+					for cur2 in cur.things_thread_pool:
+						print("Thing \"" + ucr2.ID + "\"")
+					print("-------------------------------------------------")
+
+			elif bash == "create" :
+				bash = input("create things or applications ? \n 1 ) things \n 2 ) Application ")
+
+				if bash == "1" :
+					if len(sponsor.application_thread_pool) < 1:
+						print("Sorry its supposed to have at leat one application")
+						continue
+
+					bash = input("Which application you want to add thing on ? ")
+					counter = 0
+					for cur in sponsor.application_thread_pool:
+						print(counter + " ) " + str(cur.ID) ) 
+
+					try:
+						bash = input("command $ ")
+						number_tmp2 = int(bash)
+						if not(number_tmp2 < len(sponsor.application_thread_pool) and number_tmp2 => 0):
+							print("HA HA  :D i knew it!")
+							continue
+						bash = input("How many things do you want to append ? ")
+						number_tmp3 = int(bash)
+						for cur4 in range(0, number_tmp3):
+							t = Thing()
+							sponsor.things_thread_pool.appned(t)
+							sorted(sponsor.application_thread_pool)[number_tmp2].things_thread_pool.appned(t)
+
+					except:
+						print("HA HA  :D i knew it!")
+						print(sys.info_exe())
+						continue
+
+					bash = input("How many things ? ")
+					try:
+						number_tmp = int(bash)
+						for cur in range(0,number_tmp):
+							sponsor.things_thread_pool.appned(Thing())
+					except:
+						print(sys.info_exe())
+						continue
+					
+
+				elif bash == "2":
+					sponsor.application_thread_pool.appned(Application())
+					print("Woow! you just added new application ;)")
+				else :
+					print("Ha Ha :D ")
+					continue
+			else :
+				print("Ha Ha :D")
+				continue
+		except:
+			print("Ha Ha :D your first Tyr chtched!")
+			continue
+
+
+if __name__ == '__main__':
+	sponsors = []
+	sponsors.appned(Sponsor())
+	Terminal(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
