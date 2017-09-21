0df193ea98e3d6fcfd5ad65dbd919a4be3b01d58
alirezainfotech <alireza.infotech@gmail.com>
2017-09-21 20:31:04 +0430

    ckbds

--- pro.py
+++ pro.py
@@ -19,46 +19,49 @@ class Sponsor(threading.Thread):
 
 
 	"""
-	ID = randint(0,999999)
-	type = APPType.A
-	application_thread_pool = []
-	things_thread_pool = []
-
-	pass
-
-
-class Apllication(threading.Thread):
-	"""
-
-	"""
-	type = APPType.A
-	things_thread_pool = []
-
 	def __init__(self):
+		self.ID = randint(0,999999)
+		self.type = APPType.A
+		self.application_thread_pool = []
+		self.things_thread_pool = []
+
+	def __lt__(self, other):
+		return self.ID > other.ID
+
+
+class Application(threading.Thread):
+	"""
+
+	"""
+	def __init__(self):
+		self.ID = randint(100000,999999)
+		self.type = APPType.A
+		self.things_thread_pool = []
 		self.evaluate()
+		
 
 	def evaluate(self):
-		for cur in things_thread_pool:
+		for cur in self.things_thread_pool:
 			cur.value = rt(1 , 11)
 
-	# def __gt__(self):
-	# 	pass
+	def __lt__(self, other):
+		return self.ID > other.ID
 
 class Thing(threading.Thread):
 	"""
 
 	"""
-	ID = randint(0,999999)
-	type = Type.A
-	value = 0
+	def __init__(self):
+		self.ID = randint(100000,999999)
+		self.type = Type.A
+		self.value = 0
 
+	def __lt__(self, other):
+		return self.ID > other.ID
 
 
 def Terminal(sponsors):
-	if len(sponsors) == 0:
-		print("its suppoed you created at leat one sponsor!")
-		return
-	sponsor = sponsors[0]
+
 	bash = ""
 	while True:
 		try:
@@ -66,73 +69,139 @@ def Terminal(sponsors):
 			if bash == "":
 				continue
 			elif bash == "topo":
-				apps = sorted(sponsor.application_thread_pool)
-				for cur in apps:
-					print ("Application \"" + cur.ID + "\" has following thins : ")
-					for cur2 in cur.things_thread_pool:
-						print("Thing \"" + ucr2.ID + "\"")
-					print("-------------------------------------------------")
+				if len(sponsors) == 0 :
+					print("Ooops! Nothing to show my lord!")
+					continue
+				for cur11 in sorted(sponsors):
+					print("||||||||||||||||||| Sponsor \"" + str(cur11.ID) + "\" has following thins |||||||||||||||||||")
+					apps = sorted(cur11.application_thread_pool)
+					if len(cur11.application_thread_pool) == 0 : 
+						continue
+					for cur in apps:
+						print("---------------Application \"" + str(cur.ID) + "\" has following thins -------------------")
+						for cur2 in sorted(cur.things_thread_pool):
+							print("Thing \"" + str(cur2.ID) + "\"")
+					print("---------------------------------------------------------------------------")
+				print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 
 			elif bash == "create" :
-				bash = input("create things or applications ? \n 1 ) things \n 2 ) Application ")
+				bash = input("Which one do you wanna create ? \n1 ) Sponsors \n2 ) Application \n3 ) Things \n? ")
 
-				if bash == "1" :
-					if len(sponsor.application_thread_pool) < 1:
-						print("Sorry its supposed to have at leat one application")
+				if bash == "3" :
+					if len(sponsors) < 1:
+						print("Sorry its supposed to have at leat one sponsor")
 						continue
 
-					bash = input("Which application you want to add thing on ? ")
+					print ("------------------- You have following sponsor(s) ----------------------")
 					counter = 0
-					for cur in sponsor.application_thread_pool:
-						print(counter + " ) " + str(cur.ID) ) 
+					for cur in sorted(sponsors):
+						print(str(counter) + " ) " + str(cur.ID) )
+						counter = counter + 1
+
+					print ("--------------------------------------------------------------------------")
+					bash = input("Which sponsor you want to add thing on ? ")
 
 					try:
-						bash = input("command $ ")
-						number_tmp2 = int(bash)
-						if not(number_tmp2 < len(sponsor.application_thread_pool) and (number_tmp2 > 0 or number_tmp2 == 0 )):
-							print("HA HA  :D i knew it!")
+						number_tmp4 = int(bash)
+						if not(number_tmp4 < len(sponsors) and (number_tmp4 > 0 or number_tmp4 == 0 )):
+							print("HA HA  :D i knew it! (9)")
 							continue
-						bash = input("How many things do you want to append ? ")
+						if len(sorted(sponsors)[number_tmp4].application_thread_pool) < 1:
+							print("Sorry its supposed to have at leat one application in this sponsor")
+							continue
+
+						counter2 = 0
+						print ("------------------- You have following sponsor(s) ----------------------")
+						for cur12 in sorted(sorted(sponsors)[number_tmp4].application_thread_pool):
+							print(str(counter2) + " ) " + str(cur12.ID) )
+							counter2 = counter2 + 1
+
+						print ("--------------------------------------------------------------------------")
+
+
+						bash = input("Which application you want to add thing on ? ")
+
+						try:
+							number_tmp2 = int(bash)
+							if not(number_tmp2 < len(sorted(sponsors)[number_tmp4].application_thread_pool) and (number_tmp2 > 0 or number_tmp2 == 0 )):
+								print("HA HA  :D i knew it! (6)")
+								continue
+							bash = input("How many things do you want to append ? ")
+							number_tmp3 = int(bash)
+							for cur4 in range(0, number_tmp3):
+								t = Thing()
+								sorted(sponsors)[number_tmp4].things_thread_pool.append(t)
+								sorted(sorted(sponsors)[number_tmp4].application_thread_pool)[number_tmp2].things_thread_pool.append(t)
+							
+						except:
+							print("HA HA  :D i knew it! (5)")
+							print(sys.exc_info())
+							continue
+
+					except:
+							print("HA HA  :D i knew it! (10)")
+							print(sys.exc_info())
+							continue
+					
+				elif bash == "2":
+					if len(sponsors) < 1:
+						print("Sorry its supposed to have at leat one sponsor")
+						continue
+
+					print ("------------------- You have following Sponsor(s) ----------------------")
+					counter = 0
+					for cur in sorted(sponsors):
+						print(str(counter) + " ) " + str(cur.ID) )
+						counter = counter + 1
+
+					print ("--------------------------------------------------------------------------")
+					bash = input("Which sponsor you want to add application on ? ")
+
+					try:
+						number_tmp2 = int(bash)
+						if not(number_tmp2 < len(sponsors) and (number_tmp2 > 0 or number_tmp2 == 0 )):
+							print("HA HA  :D i knew it! (7)")
+							continue
+						bash = input("How many application(s) do you want to append ? ")
 						number_tmp3 = int(bash)
 						for cur4 in range(0, number_tmp3):
-							t = Thing()
-							sponsor.things_thread_pool.appned(t)
-							sorted(sponsor.application_thread_pool)[number_tmp2].things_thread_pool.appned(t)
+							t = Application()
+							sorted(sponsors)[number_tmp2].application_thread_pool.append(t)
 
+						print("Done!")
 					except:
-						print("HA HA  :D i knew it!")
-						print(sys.info_exe())
+						print("HA HA  :D i knew it! (8)")
+						print(sys.exc_info())
 						continue
+				elif bash == "1":
+					bash = input("How many sponsor(s) do you want to append ? ")
+					number_tmp3 = int(bash)
+					for cur4 in range(0, number_tmp3):
+						t = Sponsor()
+						sponsors.append(t)
+					print("Done!")
 
-					bash = input("How many things ? ")
-					try:
-						number_tmp = int(bash)
-						for cur in range(0,number_tmp):
-							sponsor.things_thread_pool.appned(Thing())
-					except:
-						print(sys.info_exe())
-						continue
-					
-
-				elif bash == "2":
-					sponsor.application_thread_pool.appned(Application())
-					print("Woow! you just added new application ;)")
 				else :
-					print("Ha Ha :D ")
+					print("Ha Ha :D (2)")
 					continue
+			# elif bash.lower() == "load file" : 
+			# 	bash = input("Enter file path : ")
+			# 	file  = open(bash)
+
+
+				# try :
+				# 	file = open(bash)
 			elif bash.lower() == "q" :
 				break
 			else :
-				print("Ha Ha :D")
+				print("Ha Ha :D (3)")
 				continue
-		except:
-			print("Ha Ha :D your first Tyr chtched!")
-			continue
+		except KeyboardInterrupt:
+			break
 
 
 if __name__ == '__main__':
 	sponsors = []
-	sponsors.append(Sponsor())
 	Terminal(sponsors)
 
 
