5dd868e2be3345ce6e080daf30eb0a4df26b7ef7
amirrj4 <amir.j.1994@gmail.com>
2017-10-12 15:58:49 +0330

    kclacl

--- pro.py
+++ pro.py
@@ -1,6 +1,7 @@
 import threading
+from random import *
 from enum import Enum
-from random import randint
+
 import sys
 
 
@@ -9,13 +10,25 @@ class Type(Enum):
     B = 1
     C = 2
     D = 3
-
-
 class APPType(Enum):
     A = 0
     B = 1
     C = 2
     D = 3
+
+def EnergyType(En):
+     """for cur in range(1,En):
+"""
+'''
+def Valuation():
+
+from thing_thread_ pool evaluation
+if thing's type is unplugged (battery) : there is an increasing function
+if thing's type in plugged : 0
+
+
+'''
+
 
 
 class Sponsor(threading.Thread):
@@ -25,7 +38,7 @@ class Sponsor(threading.Thread):
 	"""
 
     def __init__(self):
-        self.ID = randint(0, 999999)
+        self.ID = randint(1000, 9999)
         self.application_thread_pool = []
         self.things_thread_pool = []
 
@@ -39,17 +52,22 @@ class Application(threading.Thread):
 	"""
 
     def __init__(self):
-        self.ID = randint(100000, 999999)
-        self.type = APPType.A
-        self.things_thread_pool = []
+        self.ID = randint(1000, 9999)
+        self.type = choice(['A','B','C','D'])
+        self.things_thread_pool =[]
         self.evaluate()
+
 
     def evaluate(self):
         for cur in self.things_thread_pool:
-            cur.value = rt(1, 11)
+            cur.value = randint(1, 11)
 
     def __lt__(self, other):
         return self.ID > other.ID
+
+    def q(self):
+        for cur1 in self.things_thread_pool:
+            print(cur1.value)
 
 
 class Thing(threading.Thread):
@@ -58,24 +76,29 @@ class Thing(threading.Thread):
 	"""
 
     def __init__(self):
-        self.ID = randint(100000, 999999)
-        self.type = Type.A
+        self.ID = randint(1000, 9999)
+        self.type = choice(['A','B','C','D'])
         self.value = 0
+        self.EnType = 'Battery'
 
     def __lt__(self, other):
         return self.ID > other.ID
 
 
+
+
 def Terminal(sponsors):
     bash = ""
+    print("Plesae type  \"help\" to see how can you use the system ")
     while True:
         try:
+
             bash = input("command $ ")
             if bash == "":
                 continue
             elif bash == "topo":
                 if len(sponsors) == 0:
-                    print("Ooops! Nothing to show my lord!")
+                    print("Nothing to show ! ")
                     continue
                 for cur11 in sorted(sponsors):
                     print(
@@ -96,7 +119,7 @@ def Terminal(sponsors):
 
                 if bash == "3":
                     if len(sponsors) < 1:
-                        print("Sorry its supposed to have at leat one sponsor")
+                        print("Sorry its supposed to have at least one sponsor")
                         continue
 
                     print("------------------- You have following Sponsor(s) ----------------------")
@@ -111,10 +134,10 @@ def Terminal(sponsors):
                     try:
                         number_tmp4 = int(bash)
                         if not (number_tmp4 < len(sponsors) and (number_tmp4 > 0 or number_tmp4 == 0)):
-                            print("HA HA  :D i knew it! (9)")
+                            print("The one that you chose is not in the list !")
                             continue
                         if len(sorted(sponsors)[number_tmp4].application_thread_pool) < 1:
-                            print("Sorry its supposed to have at leat one application in this sponsor")
+                            print("Sorry its supposed to have at least one application in this sponsor")
                             continue
 
                         counter2 = 0
@@ -131,29 +154,62 @@ def Terminal(sponsors):
                             number_tmp2 = int(bash)
                             if not (number_tmp2 < len(sorted(sponsors)[number_tmp4].application_thread_pool) and (
                                     number_tmp2 > 0 or number_tmp2 == 0)):
-                                print("HA HA  :D i knew it! (6)")
+                                print("The one that you chose is not in the list !")
                                 continue
                             bash = input("How many things do you want to append ? ")
                             number_tmp3 = int(bash)
+                            bash = input( "Percentage of Plugged Devices ? ")
+                            pluged_num= int(bash)
+                            if (pluged_num>100 or pluged_num<0):
+                                print("you have to choose between 0 and 100")
+                                continue
+
+                            pluged_num =int((pluged_num*number_tmp3)/100)
+                            counter1 = 0
+
                             for cur4 in range(0, number_tmp3):
                                 t = Thing()
                                 sorted(sponsors)[number_tmp4].things_thread_pool.append(t)
                                 sorted(sorted(sponsors)[number_tmp4].application_thread_pool)[
                                     number_tmp2].things_thread_pool.append(t)
 
+
+                            '''for cur5 in range(0,pluged_num):
+                                sorted(sorted(sponsors)[number_tmp4].application_thread_pool)[
+                                    number_tmp2].things_thread_pool.EnType = 'Plugged'
+                            '''
+                            for cur20 in sorted(sponsors):
+                                for cur21 in sorted(cur20.application_thread_pool):
+                                    for cur22 in sorted(cur21.things_thread_pool):
+                                        while (counter1<pluged_num):
+                                            cur22.EnType='plugged'
+                                            counter1=counter1+1
+                                            '''print(cur22.EnType)
+
+                            print("--------------------------------------------------------------------------")
+
+                            counter4 =pluged_num
+                            for cur in sorted(sponsors):
+                                for cur2 in sorted(cur.application_thread_pool):
+                                    for cur3 in sorted(cur2.things_thread_pool):
+                                        if(counter4<30):
+                                            print(str(cur3.EnType))
+                                            counter4 =counter4+1
+                                        '''
+
                         except:
-                            print("HA HA  :D i knew it! (5)")
+                            print("Error (5)")
                             print(sys.exc_info())
                             continue
 
                     except:
-                        print("HA HA  :D i knew it! (10)")
+                        print("Error (10)")
                         print(sys.exc_info())
                         continue
 
                 elif bash == "2":
                     if len(sponsors) < 1:
-                        print("Sorry its supposed to have at leat one sponsor")
+                        print("Sorry its supposed to have at least one sponsor")
                         continue
 
                     print("------------------- You have following Sponsor(s) ----------------------")
@@ -168,7 +224,7 @@ def Terminal(sponsors):
                     try:
                         number_tmp2 = int(bash)
                         if not (number_tmp2 < len(sponsors) and (number_tmp2 > 0 or number_tmp2 == 0)):
-                            print("HA HA  :D i knew it! (7)")
+                            print("The one that you chose is not in the list !")
                             continue
                         bash = input("How many application(s) do you want to append ? ")
                         number_tmp3 = int(bash)
@@ -178,7 +234,7 @@ def Terminal(sponsors):
 
                         print("Done!")
                     except:
-                        print("HA HA  :D i knew it! (8)")
+                        print("Error (8)")
                         print(sys.exc_info())
                         continue
                 elif bash == "1":
@@ -189,20 +245,55 @@ def Terminal(sponsors):
                         sponsors.append(t)
                     print("Done!")
 
+
+
+
                 else:
-                    print("Ha Ha :D (2)")
+                    print("Choose from the list please")
                     continue
                 # elif bash.lower() == "load file" :
                 # 	bash = input("Enter file path : ")
                 # 	file  = open(bash)
 
+            elif bash == "evaluate":
 
-                # try :
+                for cur11 in sorted(sponsors):
+                    apps = sorted(cur11.application_thread_pool)
+                    if len(cur11.application_thread_pool) == 0:
+                        continue
+                    for cur in apps:
+                        print("---------------Application \"" + str(
+                            cur.ID) + "\" has following thins -------------------")
+                        for cur2 in sorted(cur.things_thread_pool):
+
+                            print("Thing \"" + str(cur2.ID) + "\"")
+                    print("---------------------------------------------------------------------------")
+
+            elif bash == "help":
+                print (" \"create\"  to create Sponsor/Application/Thing ")
+                print (" \"topo\" to view designed system")
+                print (" \"quit\" to exit from the system")
+                print (" \"help\" to show more information about using the system" )
+
+
+
+
+
+                    #for cur4 in range(0, number_tmp3):
+                 #   t = Application()
+                  #  sorted(sponsors)[number_tmp2].application_thread_pool.append(t)
+
+
+
+
+
+
+                    # try :
                 # 	file = open(bash)
-            elif bash.lower() == "q":
+            elif bash.lower() == "quit":
                 break
             else:
-                print("Ha Ha :D (3)")
+                print("Wrong command ! ")
                 continue
         except KeyboardInterrupt:
             break
@@ -211,3 +302,11 @@ def Terminal(sponsors):
 if __name__ == '__main__':
     sponsors = []
     Terminal(sponsors)
+
+
+
+
+
+
+
+
